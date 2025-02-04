abffda605050a7d6aa6d68c48a5dc66f9d09172d
Björn Ricks <bjoern.ricks@greenbone.net>
2025-02-04 10:42:08 +0100

    Change: Load linter config from tools.autohooks.plugins.ruff.check
    The library now contains two precommit hooks. One for linting and one
    for formatting. Therefore the config for the linter will be loaded from
    the TOML `tools.autohooks.plugins.ruff.check` table and fallback to the
    old `tools.autohooks.plugins.ruff` table if the new one doesn't exist.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,11 @@
 import unittest
 from unittest.mock import patch
 
+from autohooks.config import AutohooksConfig
 from autohooks.plugins.ruff.utils import (
     check_ruff_installed,
     get_ruff_arguments,
+    get_ruff_config,
 )
 
 
@@ -12,6 +14,21 @@ class GetRuffArgumentsTestCase(unittest.TestCase):
         args = get_ruff_arguments(None, ["foo", "bar"])
         self.assertEqual(args, ["foo", "bar"])
         self.assertIsInstance(args, list, "ensures args is a list")
+
+    def test_get_ruff_arguments(self):
+        config = AutohooksConfig.from_string(
+            "arguments = ['--test', 'foo,bar', '--foo', 'bar']"
+        )
+        args = get_ruff_arguments(config.get_config(), ["foo", "bar"])
+        self.assertEqual(
+            args,
+            ["--test", "foo,bar", "--foo", "bar"],
+        )
+
+    def test_get_ruff_arguments_is_list(self):
+        config = AutohooksConfig.from_string("arguments = 'foo'")
+        args = get_ruff_arguments(config.get_config(), ["foo", "bar"])
+        self.assertEqual(args, ["foo"])
 
 
 class CheckRuffInstalledTestCase(unittest.TestCase):
@@ -25,3 +42,35 @@ class CheckRuffInstalledTestCase(unittest.TestCase):
             patch("importlib.util.find_spec", return_value=None),
         ):
             check_ruff_installed()
+
+
+class GetRufConfigTestCase(unittest.TestCase):
+    def test_get_empty_ruff_config(
+        self,
+    ):
+        autohooks_config = AutohooksConfig.from_string(
+            """
+            """
+        )
+
+        config = get_ruff_config(autohooks_config.get_config())
+        self.assertTrue(config.is_empty())
+
+    def test_get_ruff_config(
+        self,
+    ):
+        autohooks_config = AutohooksConfig.from_string(
+            """
+            [tool.autohooks.plugins.ruff]
+            arguments = [
+                "--test", "foo,bar",
+                "--foo", "bar",
+            ]
+            """
+        )
+
+        config = get_ruff_config(autohooks_config.get_config())
+        self.assertEqual(
+            config.get_value("arguments"),
+            ["--test", "foo,bar", "--foo", "bar"],
+        )
